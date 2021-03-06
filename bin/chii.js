986acc5d2a1e7ef83d567331ce814cd603b52e7c
owaiskreifeh <Owais.AbuKreefeh@mbc.net>
2021-03-06 23:26:01 +0200

    trying and trying to make it start

--- bin/chii.js
+++ bin/chii.js
@@ -1,34 +1,4 @@
 #!/usr/bin/env node
 
-const program = require('commander');
 const server = require('../');
-const version = require('../package.json').version;
-
-program.version(version);
-
-program
-  .command('start')
-  .description('starts chi server')
-  .option('-p, --port <port>', 'set the port to start on. defaults to 3000', parseInt)
-  .option('-h, --host <host>', 'set the host. defaults to 0.0.0.0')
-  .option('-d, --domain <domain>', 'set the domain. defaults to localhost:port')
-  .action(({ port, host, domain }) => {
-    server.start({
-      port,
-      host,
-      domain,
-    });
-  });
-
-program
-  .command('help [command]')
-  .description('display help information for a command')
-  .action(command => {
-    let cmd = program.commands.find(c => c.name() === command) || program;
-    cmd.help();
-  });
-
-const args = process.argv;
-if (args[2] === '--help' || args[2] === '-h') args[2] = 'help';
-
-program.parse(args);
+server.start();
--- server/index.js
+++ server/index.js
@@ -5,23 +5,18 @@ const compress = require('./middle/compress');
 const util = require('./lib/util');
 const WebSocketServer = require('./lib/WebSocketServer');
 
-function start({ port = 8080, host, domain, server } = {}) {
-  domain = domain || 'localhost:' + port;
+function start() {
+  const port = process.env.PORT;
+  const domain = domain || 'localhost:' + port;
 
   const app = new Koa();
   const wss = new WebSocketServer();
 
   app.use(compress()).use(router(wss.channelManager, domain));
 
-  if (server) {
-    server.on('request', app.callback());
-    wss.start(server);
-  } else {
-    util.log(`starting server at ${domain}`);
-    const server = app.listen(port, host);
-
-    wss.start(server);
-  }
+  util.log(`starting server at ${domain}`);
+  const server = app.listen(port);
+  wss.start(server);
 }
 
 module.exports = {
